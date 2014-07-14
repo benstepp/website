f40f19f9a12485c3ab10ce34609c394a1c661af9
benstepp <me@benstepp.com>
2014-07-14 16:49:50 -0500

    en-US vs en-GB
    europe region + english language will result in 24hr time format.

--- public/js/controllers/HeaderCtrl.js
+++ public/js/controllers/HeaderCtrl.js
@@ -1,7 +1,7 @@
 angular.module('HeaderCtrl',[]).controller('HeaderController', function($scope) {
 
 	$scope.locales = {
-		'English':'en',
+		'English':'en-US',
 		'French':'fr',
 		'German':'de'
 	};
@@ -15,11 +15,20 @@ angular.module('HeaderCtrl',[]).controller('HeaderController', function($scope) 
 	//default to US region add cookie checking later
 	$scope.changeRegion = function(region) {
 		$scope.data.region = region;
+		$scope.checkLocale();
 	};
 
 	$scope.changeLanguage = function(language) {
 		$scope.data.language = language;
 		$scope.data.locale = $scope.locales[language];
+		$scope.checkLocale();
+	};
+
+	//check difference in time format for US vs GB
+	$scope.checkLocale = function() {
+		if ($scope.data.language === 'English'	&& $scope.data.region !== 'US') {
+			$scope.data.locale = 'en-GB';
+		}
 	};
 
 });
