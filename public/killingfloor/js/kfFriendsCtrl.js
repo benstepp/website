e6879e9835f8ba09806acb17dafbea920c561d82
benstepp <me@benstepp.com>
2014-08-14 12:04:50 -0500

    forgot to add this controller to branch

---
+++ public/killingfloor/js/kfFriendsCtrl.js
@@ -0,0 +1,16 @@
+(function() {
+	angular
+		.module('kfFriendsCtrl',['KillingFloorService', 'SteamService', 'ui.router'])
+		.controller('kfFriendsController', ['$scope', 'friends', 'KillingFloorService', 'SteamService', kfFriendsController]);
+
+	function kfFriendsController($scope, friends, KillingFloorService, SteamService) {
+
+		//array of friends
+		$scope.friends = [];
+		//pushes the friends sorted by key in service to array for DOM binding
+		angular.forEach(friends, function(val,key) {
+			$scope.friends.push(val);
+		});
+
+	}
+})();
