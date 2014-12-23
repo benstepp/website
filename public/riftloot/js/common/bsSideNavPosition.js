5178459937f9858d4c85e7b37afb2422079ab405
benstepp <me@benstepp.com>
2014-12-23 17:33:06 -0600

    Fixed bug when resizing window with side-nav

--- public/riftloot/js/common/bsSideNavPosition.js
+++ public/riftloot/js/common/bsSideNavPosition.js
@@ -15,19 +15,20 @@
 
             function link(scope, element, attrs) {
 
-                var moveSideNav = function() {
+                function moveSideNav() {
                     var mobile = moveElement();
                     if (!mobile) {
+                        if (typeof scope.topVal==='undefined') {
+                            scope.topVal = parseInt($window.getComputedStyle(element[0], null).top);
+                        }
+
                         var scrollDistance = $window.pageYOffset;
                         //make sure the top value is either positive or 0
-                        var newTop = (scope.init - scrollDistance)>0 ? (scope.init - scrollDistance) : 0;
+                        var newTop = (scope.topVal - scrollDistance)>0 ? (scope.topVal - scrollDistance) : 0;
                         element.css({top:newTop+'px'});
                     }
-                    else {
-                        element.css({top:'0px'});
-                    }
 
-                };
+                }
 
                 function moveElement() {
                     var mobileParent = angular.element($window.document.querySelector('.inner-wrap'));
@@ -49,11 +50,10 @@
                 }
 
                 //initialize if it hasn't been
-                if (typeof scope.init === 'undefined') {
+                function init() {
                     //gets the base height of 'top' css value for side-nav
-                    scope.init = parseInt($window.getComputedStyle(element[0], null).top);
                     scope.parent = angular.element(element[0]).parent();
-
+                    moveSideNav();
 
                     //push the footer if there is one present
                     if($window.document.getElementsByTagName('footer')[0]) {
@@ -71,6 +71,7 @@
                     });
 
                 }
+                init();
             }
 
         }
