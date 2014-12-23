9088c9bd42ee6c7c1e1752d11d065698cead4049
benstepp <me@benstepp.com>
2014-12-23 16:08:01 -0600

    side-nav for LootByRole now has mobile support

--- public/riftloot/js/common/bsSideNavPosition.js
+++ public/riftloot/js/common/bsSideNavPosition.js
@@ -22,10 +22,19 @@
                     element.css({top:newTop+'px'});
                 };
 
+                var moveElement = function() {
+
+                };
+
                 //initialize if it hasn't been
                 if (typeof scope.init === 'undefined') {
                     //gets the base height of 'top' css value for side-nav
                     scope.init = parseInt($window.getComputedStyle(element[0], null).top);
+
+                    //make a copy of the entire element and put it in the left-side-nav incase of mobile
+                    var newParent = angular.element($window.document.querySelector('.inner-wrap'));
+                    newParent.append(element[0]);
+                    
 
                     //push the footer if there is one present
                     if($window.document.getElementsByTagName('footer')[0]) {
