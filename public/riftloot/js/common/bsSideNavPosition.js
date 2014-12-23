4722325e5968d2f8790d5ee2ad4d4fc37bd88a50
benstepp <me@benstepp.com>
2014-12-23 16:48:35 -0600

    Side-nav now actually works in both small and large screens

--- public/riftloot/js/common/bsSideNavPosition.js
+++ public/riftloot/js/common/bsSideNavPosition.js
@@ -16,25 +16,44 @@
             function link(scope, element, attrs) {
 
                 var moveSideNav = function() {
-                    var scrollDistance = $window.pageYOffset;
-                    //make sure the top value is either positive or 0
-                    var newTop = (scope.init - scrollDistance)>0 ? (scope.init - scrollDistance) : 0;
-                    element.css({top:newTop+'px'});
+                    var mobile = moveElement();
+                    if (!mobile) {
+                        var scrollDistance = $window.pageYOffset;
+                        //make sure the top value is either positive or 0
+                        var newTop = (scope.init - scrollDistance)>0 ? (scope.init - scrollDistance) : 0;
+                        element.css({top:newTop+'px'});
+                    }
+                    else {
+                        element.css({top:'0px'});
+                    }
+
                 };
 
-                var moveElement = function() {
+                function moveElement() {
+                    var mobileParent = angular.element($window.document.querySelector('.inner-wrap'));
+                    var parent = angular.element(element[0]).parent();
+                    var elem = angular.element(element[0]);
 
-                };
+                    //foundation medium down breakpoint
+                    if ($window.innerWidth <= 1024 && parent !== mobileParent) {
+                        elem.addClass('left-off-canvas-menu');
+                        mobileParent.append(elem);
+                        return true;
+                    }
+                    else {
+                        elem.removeClass('left-off-canvas-menu');
+                        scope.parent.append(elem);
+                        return false;
+                    }
+
+                }
 
                 //initialize if it hasn't been
                 if (typeof scope.init === 'undefined') {
                     //gets the base height of 'top' css value for side-nav
                     scope.init = parseInt($window.getComputedStyle(element[0], null).top);
+                    scope.parent = angular.element(element[0]).parent();
 
-                    //make a copy of the entire element and put it in the left-side-nav incase of mobile
-                    var newParent = angular.element($window.document.querySelector('.inner-wrap'));
-                    newParent.append(element[0]);
-                    
 
                     //push the footer if there is one present
                     if($window.document.getElementsByTagName('footer')[0]) {
