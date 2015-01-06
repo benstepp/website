0679c7465e21125b72b2b404d28f242c35233445
benstepp <me@benstepp.com>
2015-01-06 11:24:18 -0600

    Can now toggle pvp shards and region correctly

--- public/riftevents/js/controllers/SettingsController.js
+++ public/riftevents/js/controllers/SettingsController.js
@@ -10,12 +10,34 @@
 
         _this.data = {};
         _this.toggleRegion = toggleRegion;
+        _this.togglePvp = togglePvp;
+
         init();
 
+        
+
         function toggleRegion(reg) {
-        	console.log(_this.data.region[reg]);
+        	var pvpShards = ['Seastone','Bloodiron'];
+
         	angular.forEach(_this.data.region[reg].shard, function(v,k) {
-        		_this.data.region[reg].shard[k] = _this.data.region[reg].act;
+        		//if pvp is enabled just toggle all shards
+        		if (_this.data.pvp) {
+        			_this.data.region[reg].shard[k] = _this.data.region[reg].act;
+        		}
+        		//if pvp is disabled only toggle shards that aren't pvp
+        		else if (!_this.data.pvp && pvpShards.indexOf(k) === -1) {
+        			_this.data.region[reg].shard[k] = _this.data.region[reg].act;
+        		}
+        	});
+        }
+
+        function togglePvp() {
+        	//for both regions
+        	angular.forEach(_this.data.region, function(v,k) {
+        		if(_this.data.region[k].act) {
+        			if (typeof v.shard.Seastone !== 'undefined') { v.shard.Seastone = _this.data.pvp;}
+        			if (typeof v.shard.Bloodiron !== 'undefined') {v.shard.Bloodiron = _this.data.pvp;}
+        		}
         	});
         }
 
