5636c07a4e8c5dbe055b30a66f5d62515b642342
benstepp <me@benstepp.com>
2015-01-07 19:54:00 -0600

    start of translation riftevents app interface

---
+++ public/riftevents/js/common/translations.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('translations',['pascalprecht.translate'])
+        .config(['$translateProvider',translations]);
+
+    function translations($translateProvider) {
+        $translateProvider.translations('de', {
+        	EVENTS:'Geschehen',
+        	SETTINGS:'Einstellungen',
+        	LANGUAGE:'Sprache',
+        	DESKTOP_NOTIFICATIONS:'Desktop-Benachrichtigungen',
+        	EXAMPLE_NOTIFICATION:'Beispiel Benachrichtigung',
+        	SHARDS:'Shards',
+        });
+
+        $translateProvider.translations('fr', {
+        	EVENTS:'événements',
+        	SETTINGS:'Paramètres',
+        	LANGUAGE:'Langue',
+        	DESKTOP_NOTIFICATIONS:'Notifications de Bureau',
+        	EXAMPLE_NOTIFICATION:'Par Exemple la Notification',
+        	SHARDS:'Shards',
+        });
+
+        $translateProvider.translations('en', {
+        	EVENTS:'Events',
+        	SETTINGS:'Settings',
+        	LANGUAGE:'Sprache',
+        	DESKTOP_NOTIFICATIONS:'Desktop Notifications',
+        	EXAMPLE_NOTIFICATION:'Example Notification',
+        	SHARDS:'Shards',
+        });
+
+        $translateProvider.preferredLanguage('en');
+    }
+})();
