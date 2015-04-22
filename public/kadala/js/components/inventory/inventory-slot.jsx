f8250ca748e1c279b7b9c8c41ad8cc96b23c71a1
benstepp <me@benstepp.com>
2015-04-22 10:54:18 -0500

    added background images to inventory slots

--- public/kadala/js/components/inventory/inventory-slot.jsx
+++ public/kadala/js/components/inventory/inventory-slot.jsx
@@ -7,8 +7,15 @@ var InventorySlot = React.createClass({
 		var slotClass='inventory-slot';
 		//check to make sure an actual item is in the inventory slot
 		if (typeof this.props.data !== 'undefined') {
+			//change the size to large if it is a large item
 			if(this.props.data.hasOwnProperty('size') && this.props.data.size === 2) {
 				slotClass += ' large';
+			}
+
+			//set the background
+			if (this.props.data.hasOwnProperty('image')) {
+				var inline = {backgroundImage:'url('+this.props.data.image+')'};
+				slotContent.push(<div style={inline} className='inventory-image'></div>);
 			}
 		}
 
