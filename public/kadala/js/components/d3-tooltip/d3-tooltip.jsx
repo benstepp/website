655c3cbe374bda388069e949e57eea490524c5e2
benstepp <me@benstepp.com>
2015-04-23 16:43:59 -0500

    items with flavor now have it displayed on tooltip

--- public/kadala/js/components/d3-tooltip/d3-tooltip.jsx
+++ public/kadala/js/components/d3-tooltip/d3-tooltip.jsx
@@ -1,8 +1,8 @@
-var React = require('react'),
+var React = require('react');
 
-	D3ItemTooltipHead = require('./d3-tooltip-head.jsx'),
-	D3ItemTooltipBody = require('./d3-tooltip-body.jsx');
-
+var D3ItemTooltipHead = require('./d3-tooltip-head.jsx');
+var D3ItemTooltipBody = require('./d3-tooltip-body.jsx');
+var D3ItemTooltipFlavor = require('./d3-tooltip-flavor.jsx');
 
 var D3ItemTooltip = React.createClass({
 	render: function() {
@@ -10,11 +10,18 @@ var D3ItemTooltip = React.createClass({
 		if (this.props.item.rarity === 'ancient') {
 			tooltipClass+=' ancient'
 		}
+
+		//determine whether or not to add flavor
+		var flavor;
+		if (this.props.item.hasOwnProperty('flavor')) {
+			flavor = <D3ItemTooltipFlavor flavor={this.props.item.flavor} />
+		}
 		return (
 			<div className='tooltip-content'>
 				<div className={tooltipClass}>
 					<D3ItemTooltipHead item={this.props.item} />
 					<D3ItemTooltipBody item={this.props.item} />
+					{flavor}
 				</div>
 			</div>
 		);
