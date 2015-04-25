17780f5ccfef6e99ef0ea1e3d0c5c59c72be81b2
benstepp <me@benstepp.com>
2015-04-25 10:05:59 -0500

    will now only attempt to send email if password was provided in process.argv

--- server/contact/email/index.js
+++ server/contact/email/index.js
@@ -4,27 +4,40 @@ var assign = require('object-assign');
 var authConfig = require('../config/auth');
 var mailConfig = require('../config/mail');
 
-var transporter = nodemailer.createTransport({
-	service:'Gmail',
-	auth:{
-		user:authConfig.user,
-		pass:authConfig.pass
-	}
-});
-
-function email(body,callback) {
-	var options = assign({},mailConfig);
-	options.from = body.name + ' <'+body.email+'>';
-	options.text = body.message;
-
-	transporter.sendMail(options, function(err,info) {
-		if (err) {
-			callback({error:1});
-		}
-		else {
-			callback({error:0});
+//create the transporter if given a password
+var transporter;
+if (authConfig.pass) {
+	transporter = nodemailer.createTransport({
+		service:'Gmail',
+		auth:{
+			user:authConfig.user,
+			pass:authConfig.pass
 		}
 	});
+}
+
+
+function email(body,callback) {
+
+	//if the password is not set return immediately with error
+	if (!authConfig.hasOwnProperty('pass')) {
+		callback({error:'Server authentication misconfigured. Contact server administrator.'});
+	}
+
+	else {
+		var options = assign({},mailConfig);
+		options.from = body.name + ' <'+body.email+'>';
+		options.text = body.message;
+
+		transporter.sendMail(options, function(err,info) {
+			if (err) {
+				callback({error:1});
+			}
+			else {
+				callback({error:0});
+			}
+		});
+	}
 
 }
 
